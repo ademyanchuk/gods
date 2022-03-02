e6780df8cf786449f6aab496a9ee3e8a51f4007a
Alexey Demyanchuk <demyanchuk.alexey@mh-hannover.de>
2022-03-02 10:48:16 +0100

    Add build tree method + minor documentation

--- src/bstree/bstree.go
+++ src/bstree/bstree.go
@@ -1,16 +1,28 @@
 // Binary Search Tree Implementation
 package bstree
 
+// Tree Node Structure
 type Node struct {
 	Left  *Node
 	Right *Node
 	Val   int
 }
 
+// Tree Structure
 type Tree struct {
 	Root *Node
 }
 
+// Build a new Tree from `values`
+func Build(values []int) *Tree {
+	tree := &Tree{}
+	for _, val := range values {
+		tree.Insert(val)
+	}
+	return tree
+}
+
+// Insert according to BS Tree Invariant
 func (tree *Tree) Insert(value int) {
 	if tree.Root == nil {
 		tree.Root = NewNode(value)
@@ -19,10 +31,12 @@ func (tree *Tree) Insert(value int) {
 	tree.Root.Insert(value)
 }
 
+// Node constructor
 func NewNode(value int) *Node {
 	return &Node{Left: nil, Right: nil, Val: value}
 }
 
+// Insert according to BS Tree Invariant
 func (n *Node) Insert(value int) {
 	if value < n.Val {
 		if n.Left == nil {
--- src/bstree/bstree_test.go
+++ src/bstree/bstree_test.go
@@ -1,6 +1,9 @@
 package bstree
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestInsert(t *testing.T) {
 	tree := &Tree{}
@@ -22,5 +25,14 @@ func TestInsertNodes(t *testing.T) {
 	}
 	if tree.Root.Left.Val != left_val {
 		t.Errorf("got %q, want %q", tree.Root.Left.Val, left_val)
+	}
+}
+
+func TestBuild(t *testing.T) {
+	values := []int{2, 1, 3}
+	tree := Build(values)
+	got := []int{tree.Root.Val, tree.Root.Left.Val, tree.Root.Right.Val}
+	if !reflect.DeepEqual(values, got) {
+		t.Errorf("got %v, want %v", got, values)
 	}
 }
